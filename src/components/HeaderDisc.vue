90198bba422904e2b17e9aec9d722814f9f5313e
GiovanniCalia <johnca@outlook.it>
2022-03-24 17:32:07 +0100

    Creazione header

--- src/components/HeaderDisc.vue
+++ src/components/HeaderDisc.vue
@@ -1,6 +1,6 @@
 <template>
-  <header class="text-center">
-      <div></div>
+  <header class="d-flex p-3">
+      <div><i class="fa-brands fa-spotify"></i></div>
      </header>
 </template>
 
@@ -11,5 +11,13 @@ export default {
 </script>
 
 <style scoped lang='scss'>
+header{
+    background-color: rgb(46, 58, 70);
+}
+
+i{
+    color: rgb(26, 215, 96);
+    font-size: 40px;
+}
 
 </style>
