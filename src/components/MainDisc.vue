7c12c7be686e98ec10a48f156dc90b783137eade
GiovanniCalia <johnca@outlook.it>
2022-03-24 19:54:44 +0100

    Piccolo aggiornamento esercizio completato

--- src/components/MainDisc.vue
+++ src/components/MainDisc.vue
@@ -1,6 +1,6 @@
 <template>
 <main>
-    <div class="container pt-5">
+    <div class="container py-5">
         <div class="row justify-content-center">
           <div class="col-lg-2 col-md-4 col-sm-6 text-center mb-3 py-3 card_dark m-3" v-for="element in characters" :key="element">
                 <img class="img-fluid mb-3" :src="element.poster" :alt="element.author">
